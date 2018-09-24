2e4986735c211a969c9ed3b1de3c15789ea0910e
Alweezy <alvinmutisya@gmail.com>
2018-09-24 10:56:53 +0300

    Clean up code

--- api/app.py
+++ api/app.py
@@ -19,33 +19,6 @@ def create_app(config_name):
 
     db.init_app(app)
 
-    @app.route("/api/v1/questions/", methods=["POST"])
-    def get_questions():
-        if request.method == "POST":
-            title = str(request.data.get("title", ""))
-            asked_by = request.data.get("user_id")
-            question = Question(title=title, asked_by=asked_by)
-            question.save()
-            # all_questions = [question.name for question in
-            #                  Question.query.filter_by(created_by=user_id)]
-            response = jsonify({
-                "id": question.id,
-                "title": question.title,
-                "date_created": question.date_created,
-                "date_modified": question.date_modified,
-                "asked_by": question.asked_by
-            })
-            return make_response(response), 201
-
-    @app.route("/api/v1/users", methods=["POST"])
-    def create_users():
-        username = str(request.data.get("username"))
-        email = str(request.data.get("email"))
-        password = str(request.data.get("password"))
-        user = User(username=username, email=email, password=password)
-        user.save()
-        return make_response(jsonify({"message": "user created"})), 201
-
     from .auth import auth_blueprint
     app.register_blueprint(auth_blueprint)
     return app
